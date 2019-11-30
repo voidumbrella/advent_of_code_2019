398dc92a3f8224f80b47dfa408c5bf82e4be1673
voidumbrella <voidumbrella@gmail.com>
2019-11-30 01:18:48 -0500

    Project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code 2019
+I'm using this year's [Advent of Code](https://adventofcode.com) as an
+opportunity to learn Rust from the scratch.
+
+This uses the [cargo-aoc](https://github.com/gobanos/cargo-aoc) library.
+To solve a puzzle, run
+```bash
+cargo aoc [-d day] [-p part]
+```
---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+// pub mod day01;
+// pub mod day02;
+// pub mod day03;
+// pub mod day04;
+// pub mod day05;
+// pub mod day06;
+// pub mod day07;
+// pub mod day010;
+// pub mod day011;
+// pub mod day012;
+// pub mod day013;
+// pub mod day014;
+// pub mod day015;
+// pub mod day016;
+// pub mod day017;
+// pub mod day020;
+// pub mod day021;
+// pub mod day022;
+// pub mod day023;
+// pub mod day024;
+// pub mod day025;
+
+aoc_lib!{year = 2015}

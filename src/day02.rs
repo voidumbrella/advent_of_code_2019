8dea1a30688b6f1cf396a131ab4ae24bce334130
voidumbrella <voidumbrella@gmail.com>
2019-12-02 11:51:42 -0500

    Moved functions into struct methods

--- src/day02.rs
+++ src/day02.rs
@@ -23,13 +23,10 @@ fn input_generator(input: &str) -> IntCode {
     }
 } 
 
-fn execute(p: &IntCode, noun: i32, verb: i32) -> i32 {
-    let mut p = p.clone();
-    p.mem[1] = noun;
-    p.mem[2] = verb;
-    loop {
+impl IntCode {
+    fn get_args(&self) -> (usize, Vec<i32>) {
+        let opcode = self.mem[self.ip];
         let num_params;
-        let opcode = p.mem[p.ip];
 
         match opcode {
             1 => { num_params = 3; }
@@ -38,37 +35,42 @@ fn execute(p: &IntCode, noun: i32, verb: i32) -> i32 {
             _ => panic!("Invalid opcode {}", opcode),
         }
 
-        let args = p.mem[p.ip + 1..p.ip + 1 + num_params].to_vec();
-
-        match opcode {
-            1 => {
-                p.mem[args[2] as usize] =
-                    p.mem[args[0] as usize] + p.mem[args[1] as usize];
-            },
-            2 => {
-                p.mem[args[2] as usize] =
-                    p.mem[args[0] as usize] * p.mem[args[1] as usize];
-            },
-            99 => break,
-            _ => panic!("Invalid opcode {}", opcode),
-        }
-
-        p.ip += num_params + 1;
+        (num_params, self.mem[self.ip + 1..self.ip + 1 + num_params].to_vec())
     }
 
-    p.mem[0]
+    fn execute(&mut self, noun: i32, verb: i32) -> i32 {
+        self.mem[1] = noun;
+        self.mem[2] = verb;
+        loop {
+            let (num_params, args) = self.get_args();
+            let opcode = self.mem[self.ip];
+
+            match opcode {
+                1 => { self.mem[args[2] as usize] = self.mem[args[0] as usize] + self.mem[args[1] as usize]; },
+                2 => { self.mem[args[2] as usize] = self.mem[args[0] as usize] * self.mem[args[1] as usize]; },
+                99 => break,
+                _ => panic!("Invalid opcode {}", opcode),
+            }
+
+            self.ip += num_params + 1;
+        }
+
+        self.mem[0]
+    }
 }
 
 #[aoc(day2, part1)]
 fn solve_part1(input: &IntCode) -> i32 {
-    execute(&input, 12, 2)
+    let mut program = input.clone();
+    program.execute(12, 2)
 }
 
 #[aoc(day2, part2)]
 fn solve_part2(input: &IntCode) -> i32 {
     for noun in 0..100 {
         for verb in 0..100 {
-            if execute(&input, noun, verb) == 19690720 {
+            let mut program = input.clone();
+            if program.execute(noun, verb) == 19690720 {
                 return 100 * noun + verb
             }
         }
